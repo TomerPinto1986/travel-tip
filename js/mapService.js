9f6836b612cf1d65730141a405554f46d7a52428
Tomer Pinto <t.pintoosi@gmail.com>
2020-10-28 17:53:51 +0200

    merging

--- js/mapService.js
+++ js/mapService.js
@@ -1,14 +1,33 @@
 'use strict'
 
+import { storageService } from './storage-service.js'
+
+const LOCATIONS_KEY = 'locations'
+
+
+function saveLocations(tripLocations) {
+    saveToStorage(LOCATIONS_KEY, tripLocations);
+}
+
+function loadLocations() {
+    return loadFromStorge(LOCATIONS_KEY);
+}
+
+
+
+// var locs = [{ lat: 11.22, lng: 22.11 }]
+
+// function getLocs() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(locs);
+//         }, 2000)
+//     });
+// }
+
+
 export const mapService = {
     getLocs: getLocs,
-}
-var locs = [{ lat: 11.22, lng: 22.11 }]
-
-function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+    loadLocations,
+    saveLocations
 }
---
+++ js/storage-service.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    const val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
+
+
+
+export const storageService = {
+    saveToStorage,
+    loadFromStorage
+}
